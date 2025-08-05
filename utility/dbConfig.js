659df61ed60aa627805bf51dae6d41632e263433
baranncelik <121351869+baranncelik@users.noreply.github.com>
2025-08-06 01:35:42 +0300

    Update dbConfig.js

--- utility/dbConfig.js
+++ utility/dbConfig.js
@@ -1,7 +1,14 @@
-module.exports = { 
-    user : "root",
-    host : "localhost",
-    password : "BaranCelik!48650913",
-    database  : "dormitory_db"
-
+module.exports = {
+  user: "root",
+  host: "dpg-d2983cc9c44c739bps9g-a",  // Render’ın sana verdiği host
+  password: "YTkH3u2Zy1aPXfHHVklyaxtirvgMlPCv", // Şifren
+  database: "dormitory_db",
+  port: 5432, // PostgreSQL default portu
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Render için gerekli SSL ayarı
+    }
+  }
 };
